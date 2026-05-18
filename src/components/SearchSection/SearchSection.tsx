c5e398ca1798f9637faa73654cea0e824a2f903c
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-05-18 14:19:19 +0300

    feat: add frame glow animations

--- src/components/SearchSection/SearchSection.tsx
+++ src/components/SearchSection/SearchSection.tsx
@@ -59,7 +59,7 @@ export class SearchSection extends Component<
     return (
       <div className="search-section">
         <div className="search-input-wrapper">
-          <RoundedFrame className="highlighted-frame">
+          <RoundedFrame className="search-input-frame highlighted-frame">
             <input
               type="text"
               placeholder={UI_MESSAGES.SEARCH_PLACEHOLDER}

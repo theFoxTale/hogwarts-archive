9ea5edffbc88bb9906518d75ca5208cf510e6403
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-05-19 16:45:18 +0300

    refactor: convert AppHeader component to functional component

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,36 +1,33 @@
-import { Component } from 'react';
 import { APP_STRINGS } from '../../constants';
 
 import './AppHeader.css';
 import emblemIcon from '../../assets/images/hogwarts-emblem.png';
 import ornamentIcon from '../../assets/images/hogwarts-ornaments.png';
 
-export class AppHeader extends Component {
-  render() {
-    return (
-      <div className="app-header">
+export function AppHeader() {
+  return (
+    <div className="app-header">
+      <img
+        src={emblemIcon}
+        alt={APP_STRINGS.APP_EMBLEM_TOOLTIP}
+        className="app-header__emblem"
+      />
+      <p className="app-header__title magic-title">{APP_STRINGS.APP_NAME}</p>
+      <div className="app-header__description">
         <img
-          src={emblemIcon}
+          src={ornamentIcon}
           alt={APP_STRINGS.APP_EMBLEM_TOOLTIP}
-          className="app-header__emblem"
+          className="app-header__ornament"
         />
-        <p className="app-header__title magic-title">{APP_STRINGS.APP_NAME}</p>
-        <div className="app-header__description">
-          <img
-            src={ornamentIcon}
-            alt={APP_STRINGS.APP_EMBLEM_TOOLTIP}
-            className="app-header__ornament"
-          />
-          <p className="app-header__description-text magic-subtitle">
-            {APP_STRINGS.APP_DESCRIPTION}
-          </p>
-          <img
-            src={ornamentIcon}
-            alt={APP_STRINGS.APP_EMBLEM_TOOLTIP}
-            className="app-header__ornament app-header__ornament--mirrored"
-          />
-        </div>
+        <p className="app-header__description-text magic-subtitle">
+          {APP_STRINGS.APP_DESCRIPTION}
+        </p>
+        <img
+          src={ornamentIcon}
+          alt={APP_STRINGS.APP_EMBLEM_TOOLTIP}
+          className="app-header__ornament app-header__ornament--mirrored"
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }

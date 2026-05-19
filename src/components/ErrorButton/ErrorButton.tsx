f776338a7b427af0fc5e4ece024156ecd83992d1
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-05-19 16:39:35 +0300

    refactor: convert ErrorButton to functional component

--- src/components/ErrorButton/ErrorButton.tsx
+++ src/components/ErrorButton/ErrorButton.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { APP_STRINGS } from '../../constants';
 
 import './ErrorButton.css';
@@ -8,20 +7,18 @@ interface ErrorButtonProps {
   onSimulateError: () => void;
 }
 
-export class ErrorButton extends Component<ErrorButtonProps> {
-  render() {
-    return (
-      <button
-        onClick={this.props.onSimulateError}
-        className="error-test-button"
-        aria-label={APP_STRINGS.ERROR_BUTTON_TOOLTIP}
-      >
-        <img
-          src={errorIcon}
-          alt={APP_STRINGS.ERROR_BUTTON_TOOLTIP}
-          className="error-icon-img"
-        />
-      </button>
-    );
-  }
+export function ErrorButton({ onSimulateError }: ErrorButtonProps) {
+  return (
+    <button
+      onClick={onSimulateError}
+      className="error-test-button"
+      aria-label={APP_STRINGS.ERROR_BUTTON_TOOLTIP}
+    >
+      <img
+        src={errorIcon}
+        alt={APP_STRINGS.ERROR_BUTTON_TOOLTIP}
+        className="error-icon-img"
+      />
+    </button>
+  );
 }

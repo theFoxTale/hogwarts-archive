44d3b5935f93886d07717e928b4f6452bb1ea57b
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-05-25 23:30:00 +0300

    test: implement test for App component

---
+++ src/test/App.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+import selectedItemsReducer from '../features/selectedItemsSlice';
+import { ThemeProvider } from '../contexts';
+import { APP_STRINGS } from '../constants';
+
+import { App } from '../App';
+
+const createStore = () =>
+  configureStore({
+    reducer: { selectedItems: selectedItemsReducer },
+    preloadedState: { selectedItems: {} },
+  });
+
+describe('App tests', () => {
+  test('renders without crashing and shows header content', () => {
+    render(
+      <Provider store={createStore()}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Provider>
+    );
+
+    expect(screen.getByText(APP_STRINGS.APP_NAME)).toBeInTheDocument();
+  });
+});

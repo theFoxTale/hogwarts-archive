8c24dba37e52f20a7d6f16d68071a3dfd9a877bc
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-05-27 22:38:57 +0300

    feat: update AboutFlag background

--- src/components/AboutFlag/AboutFlag.tsx
+++ src/components/AboutFlag/AboutFlag.tsx
@@ -2,28 +2,41 @@ import { useNavigate } from 'react-router-dom';
 import { ABOUT_STRINGS } from '../../constants';
 
 import './AboutFlag.css';
-import flagTop from '../../assets/images/flag/flag-top.png';
-import flagMiddle from '../../assets/images/flag/flag-middle.png';
-import flagBottom from '../../assets/images/flag/flag-bottom.png';
+
+import flagTopDark from '../../assets/images/flag/dark/flag-top.png';
+import flagMiddleDark from '../../assets/images/flag/dark/flag-middle.png';
+import flagBottomDark from '../../assets/images/flag/dark/flag-bottom.png';
+
+import flagTopLight from '../../assets/images/flag/light/flag-top.png';
+import flagMiddleLight from '../../assets/images/flag/light/flag-middle.png';
+import flagBottomLight from '../../assets/images/flag/light/flag-bottom.png';
+
 import aboutIcon from '../../assets/images/flag/about-icon.png';
+import { useTheme } from '../../contexts';
 
 export function AboutFlag() {
   const navigate = useNavigate();
+  const { theme } = useTheme();
 
   return (
     <div className="about-flag" onClick={() => navigate('/about')}>
       <div className="flag-top">
-        <img src={flagTop} alt="" />
+        <img src={theme === 'light' ? flagTopLight : flagTopDark} alt="" />
       </div>
       <div
         className="flag-middle"
-        style={{ backgroundImage: `url(${flagMiddle})` }}
+        style={{
+          backgroundImage: `url(${theme === 'light' ? flagMiddleLight : flagMiddleDark})`,
+        }}
       >
         <img src={aboutIcon} alt="" className="flag-icon" />
         <span className="flag-text magic-title">{ABOUT_STRINGS.TEXT}</span>
       </div>
       <div className="flag-bottom">
-        <img src={flagBottom} alt="" />
+        <img
+          src={theme === 'light' ? flagBottomLight : flagBottomDark}
+          alt=""
+        />
       </div>
     </div>
   );

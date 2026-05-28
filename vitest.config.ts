e20908d31c436e3bbfa8c4a1197a950b00f64bf9
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-05-28 21:52:49 +0300

    chore: update vite config according vite recommendations

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
@@ -25,5 +24,8 @@ export default defineConfig({
         lines: 50,
       },
     },
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
 });

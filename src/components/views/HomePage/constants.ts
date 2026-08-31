8711b32e00bc5dbfa73873261448971c35e706d2
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-08-31 10:26:32 +0300

    fix: remove localStorage

--- src/components/views/HomePage/constants.ts
+++
@@ -1,4 +0,0 @@
-export const LOCAL_STORAGE_KEYS = {
-  SEARCH_TEXT: 'classComponentSearchText',
-  SEARCH_PAGE: 'classComponentSearchPage',
-} as const;
--- src/components/views/index.ts
+++ src/components/views/index.ts
@@ -1,5 +1,1 @@
-/* Views */
 export { HomePage } from './HomePage/HomePage';
-
-/* Constants */
-export { LOCAL_STORAGE_KEYS } from './HomePage/constants';
